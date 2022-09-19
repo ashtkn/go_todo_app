5f85e22af3140b4224fdf1a192521b5cffd6f884
Asahi Takenouchi <asahi.taken@gmail.com>
2022-09-19 16:09:14 +0900

    feat: create server.go, mux.go, and testing files.

--- main.go
+++ main.go
@@ -5,28 +5,19 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/ashtkn/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	// Handle SIGTERM
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	// Load config
 	cfg, err := config.New()
 	if err != nil {
 		return err
 	}
 
-	// Create server
+	// Create listener
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
@@ -35,27 +26,10 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("server is running at %s", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
+	mux := NewMux()
+	s := NewServer(l, mux)
 
-	<-ctx.Done()
-
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	return eg.Wait()
+	return s.Run(ctx)
 }
 
 func main() {
---
+++ mux.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func NewMux() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+	return mux
+}
