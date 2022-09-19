a5562cd4a0f72e29f9b5da2b6c87f9838250a837
Asahi Takenouchi <asahi.taken@gmail.com>
2022-09-19 18:22:14 +0900

    feat: implement router

--- mux.go
+++ mux.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/ashtkn/go_todo_app/handler"
+	"github.com/ashtkn/go_todo_app/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
 )
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
+
+	// GET /health
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(`{"status": "ok"}`))
 	})
+
+	// POST /tasks
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+
+	// GET /tasks
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
