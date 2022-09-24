7324393bbc57ddbe7defd97d1dca610bf5605d3a
Asahi Takenouchi <asahi.taken@gmail.com>
2022-09-24 14:52:50 +0900

    feat: create RegisterUser on Repository

--- store/user.go
+++ store/user.go
@@ -1,1 +1,33 @@
 package store
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/ashtkn/go_todo_app/entity"
+	"github.com/go-sql-driver/mysql"
+)
+
+func (r *Repository) RegisterUser(ctx context.Context, db Execer, u *entity.User) error {
+	u.Created = r.Clocker.Now()
+	u.Modified = r.Clocker.Now()
+
+	sql := `INSERT INTO user (name, password, role, created, modified) VALUES (?, ?, ?, ?, ?)`
+	result, err := db.ExecContext(ctx, sql, u.Name, u.Password, u.Role, u.Created, u.Modified)
+	if err != nil {
+		var mySqlError *mysql.MySQLError
+		if errors.As(err, &mySqlError) && mySqlError.Number == 1062 {
+			return fmt.Errorf("user already exists: %w", ErrAlreadyEntry)
+		}
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	u.ID = entity.UserID(id)
+
+	return nil
+}
