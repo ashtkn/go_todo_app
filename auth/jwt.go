1d908cbd29c08ba55200fb26208debbafb2c4151
Asahi Takenouchi <asahi.taken@gmail.com>
2022-09-24 17:12:24 +0900

    feat: create jwt GenerateToken function

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,9 +1,93 @@
 package auth
 
-import _ "embed"
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"time"
+
+	"github.com/ashtkn/go_todo_app/clock"
+	"github.com/ashtkn/go_todo_app/entity"
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+)
+
+const (
+	RoleKey     = "role"
+	UserNameKey = "user_name"
+)
 
 //go:embed cert/secret.pem
 var rawPrivateKey []byte
 
 //go:embed cert/public.pem
 var rawPublicKey []byte
+
+type JWTer struct {
+	PrivateKey, PublicKey jwk.Key
+	Store                 Store
+	Clocker               clock.Clocker
+}
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . Store
+type Store interface {
+	Save(ctx context.Context, key string, userId entity.UserID) error
+	Load(ctx context.Context, key string) (entity.UserID, error)
+}
+
+func NewJWTer(s Store, c clock.Clocker) (*JWTer, error) {
+	j := &JWTer{Store: s}
+	privateKey, err := parse(rawPrivateKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse private key: %w", err)
+	}
+	publicKey, err := parse(rawPublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse public key: %w", err)
+	}
+
+	j.PrivateKey = privateKey
+	j.PublicKey = publicKey
+	j.Clocker = c
+
+	return j, nil
+}
+
+func parse(rawKey []byte) (jwk.Key, error) {
+	key, err := jwk.ParseKey(rawKey, jwk.WithPEM(true))
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func (j *JWTer) GenerateToken(ctx context.Context, u entity.User) ([]byte, error) {
+	token, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer(`github.com/ashtkn/go_todo_app`).
+		Subject("access_token").
+		IssuedAt(j.Clocker.Now()).
+		// redisのexpireはこれを使う。
+		// https://pkg.go.dev/github.com/go-redis/redis/v8#Client.Set
+		// clock.Durationだから Subする必要がある
+		Expiration(j.Clocker.Now().Add(30*time.Minute)).
+		Claim(RoleKey, u.Role).
+		Claim(UserNameKey, u.Name).
+		Build()
+
+	if err != nil {
+		return nil, fmt.Errorf("GenerateToken: failed to build token: %w", err)
+	}
+	if err := j.Store.Save(ctx, token.JwtID(), u.ID); err != nil {
+		return nil, err
+	}
+
+	// Sign a JWT!
+	signed, err := jwt.Sign(token, jwt.WithKey(jwa.RS256, j.PrivateKey))
+	if err != nil {
+		return nil, err
+	}
+	return signed, nil
+}
